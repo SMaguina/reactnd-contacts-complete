519123e0d9be3d00f5d84d366e53e4518e040ed1
Sylvia Maguiña <smag23@gmail.com>
2018-03-13 13:44:08 -0700

    function to serialize form submission queries

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
 import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    screen: 'list', // list, create
     contacts: []
   }
   componentDidMount() {
@@ -20,24 +20,36 @@ class App extends Component {
 
     ContactsAPI.remove(contact)
   }
+
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
+
+
   render() {
     return (
-      <div>
-        {this.state.screen === 'list' && (
-        <ListContacts
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
-          onNavigate={() => {
-            this.setState({ screen: 'create' })
-          }}
-        />
-      )}
-      {this.state.screen === 'create' && (
-        <CreateContact/>
-      )}
+      <div className="app">
+        <Route exact path="/" render={() => (
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+          />
+        )}/>
+        <Route path="/create" render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )}/>
       </div>
     )
   }
 }
 
-export default App;
+export default App
